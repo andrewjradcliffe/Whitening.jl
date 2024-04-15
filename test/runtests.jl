511d11bb4df94525a94fa81288b4f6e4dc5558d9
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2024-04-14 20:26:10 -0700

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,36 +1,26 @@
 using Whitening, LinearAlgebra, Test, Random, Statistics
 
-@testset "basic properties" begin
-    rng = Xoshiro(0xbad_c0ffee_bad_cafe)
-    μ = Float64[5, 6, 7]
-    Σ = Float64[1 0 0; 0 2 0; 0 0 3]
-    F = eigen(Σ)
-    W⁻¹ = F.vectors * Diagonal(sqrt.(F.values))
-    F = svd(Σ)
-    W⁻¹ = F.U * Diagonal(sqrt.(F.S))
+@testset "round-tripping; kernel: $k" for k in (Whitening.PCA, Whitening.PCAcor, Whitening.ZCA, Whitening.ZCAcor, Whitening.Chol)
+    ρ₁₂ = ρ₂₁ = 0.7
+    ρ₁₃ = ρ₃₁ = 0.5
+    ρ₂₃ = ρ₃₂ = 0.3
+    σ₁ = 1.0
+    σ₂ = 0.5
+    σ₃ = 2.0
+    μ = Float64[3, 2, 1]
+    Σ = [
+        1.0*abs2(σ₁) ρ₁₂*σ₁*σ₂ ρ₁₃*σ₁*σ₃
+        ρ₂₁*σ₂*σ₁ 1.0*abs2(σ₂) ρ₂₃*σ₂*σ₃
+        ρ₃₁*σ₃*σ₁ ρ₃₂*σ₃*σ₂ 1.0*abs2(σ₃)
+    ]
+    kern = k(μ, Σ)
+    z = randn(3)
+    x = Whitening.unwhiten(kern, z)
+    z_rt = Whitening.whiten(kern, x)
+    @test all(z .≈ z_rt)
 
-    Z = rand(rng, 3, 10)
-
-    X = μ .+ W⁻¹ * Z
-    # μ = mean(X, dims=2)
-    # Σ = cov(X, dims=2, corrected=false)
-
-    K = Whitening.Kernel(vec(μ), Σ)
-
-    @testset "inner product equivalence under `eigen`" begin
-        let X = μ .+ W⁻¹ * Z, old = eachcol(Z), new = whiten.(eachcol(X), Ref(K))
-            @test all(old .⋅ old .≈ new .⋅ new)
-        end
-    end
-
-
-    F = svd(Σ)
-    W⁻¹ = F.U * Diagonal(sqrt.(F.S))
-
-    @testset "element-wise equivalence under `svd`" begin
-        let X = μ .+ W⁻¹ * Z, old = eachcol(Z), new = whiten.(eachcol(X), Ref(K))
-            @test all(old .⋅ old .≈ new .⋅ new)
-            @test all(Z .≈ stack(new, dims = 2))
-        end
-    end
+    Z = randn(3, 100)
+    X = Whitening.unwhiten(kern, Z)
+    Z_rt = Whitening.whiten(kern, X)
+    @test all(Z .≈ Z_rt)
 end
