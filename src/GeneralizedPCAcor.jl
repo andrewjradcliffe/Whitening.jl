877f8ec15001307af933189033e8b1eb22249ba8
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2024-04-22 19:52:00 -0700

    Revise interface and constructor

--- src/GeneralizedPCAcor.jl
+++ src/GeneralizedPCAcor.jl
@@ -3,10 +3,10 @@
 
 Scale-invariant principal component analysis (PCAcor) whitening transform, generalized to
 support compression based on either
-(1) a pre-determined number of components,
-(2) a fraction of the total squared cross-correlation, or
-(3) a relative tolerance on the number of eigenvalues greater than `rtol*λ₁`
-where `λ₁` is the largest eigenvalue of the correlation matrix.
+1. a pre-determined number of components,
+2. a fraction of the total squared cross-correlation, or
+3. a relative tolerance on the number of eigenvalues greater
+   than `rtol*λ₁` where `λ₁` is the largest eigenvalue of the correlation matrix.
 """
 struct GeneralizedPCAcor{T<:Base.IEEEFloat} <: AbstractWhiteningTransform{T}
     μ::Vector{T}
@@ -18,11 +18,11 @@ struct GeneralizedPCAcor{T<:Base.IEEEFloat} <: AbstractWhiteningTransform{T}
     function GeneralizedPCAcor{T}(
         μ::Vector{T},
         Σ::Matrix{T};
-        num_components::Int = size(Σ, 1),
-        ratio::T = one(T),
-        rtol::T = size(Σ, 1) * eps(T),
+        num_components::Union{Int, Nothing},
+        vmin::Union{T, Nothing},
+        rtol::Union{T, Nothing},
     ) where {T<:Base.IEEEFloat}
-        checkargs(μ, Σ, num_components, ratio)
+        checkargs(μ, Σ, num_components, vmin)
         # J, J₀ = nz_z(v)
         # v¹² = sqrt.(getindex.(Ref(v), J))
         # v⁻¹² = inv.(v¹²)
@@ -33,7 +33,7 @@ struct GeneralizedPCAcor{T<:Base.IEEEFloat} <: AbstractWhiteningTransform{T}
         V⁻¹² = Diagonal(inv.(v¹²))
         V¹² = Diagonal(v¹²)
         F = eigen(P, sortby = -)
-        n⃰ = determine_nstar(F.values, num_components, ratio, rtol)
+        n⃰ = determine_nstar(F.values, num_components, vmin, rtol)
         Λ¹² = sqrt.(@view(F.values[1:n⃰]))
         Λ⁻¹² = inv.(Λ¹²)
         B¹² = Diagonal(Λ¹²)
@@ -82,18 +82,48 @@ function nz_z(v::Vector{T}) where {T}
     end
 end
 
+"""
+    GeneralizedPCAcor(μ::AbstractVector{T}, Σ::AbstractMatrix{T};
+                   num_components::Union{Int, Nothing}=nothing,
+                   vmin::Union{T, Nothing}=nothing,
+                   rtol::Union{T, Nothing}=nothing) where {T<:Base.IEEEFloat}
+
+Construct a generalized PCAcor transformer from the mean vector, `μ` ∈ ℝⁿ,
+and a covariance matrix, `Σ` ∈ ℝⁿˣⁿ; `Σ` must be symmetric and
+positive semi-definite.
+
+The decomposition, `Σ = √V * P √V`, where `V` is the diagonal matrix
+of variances and `P` is a correlation matrix, must be well-formed
+in order to obtain a meaningful result. That is, if the diagonal of `Σ`
+contains 1 or more zero elements, then it is not possible to compute
+`P = (√V)⁻¹ * Σ * (√V)⁻¹`.
+
+
+The output dimension, `m`, of the transformer is determined from
+the optional arguments, where
+1.  0 ≤ `num_components` ≤ n is a pre-determined size
+2.  0 ≤ `vmin` ≤ 1 is the fraction of the total squared cross-covariance,
+    hence, `m` is the smallest value such that `sum(λ[1:m]) ≥ vmin*sum(λ)`,
+    where `λᵢ, i=1,…,n` are the eigenvalues of `P` in descending order.
+3. `rtol` is the relative tolerance on the number of eigenvalues greater
+   than `rtol*λ₁` where `λ₁` is the largest eigenvalue of `P`.
+
+If none of the 3 options are provided, the default is `rtol = n*eps(T)`.
+If 2 or more options are provided, the minimum of the resultant sizes will
+be chosen.
+"""
 function GeneralizedPCAcor(
     μ::AbstractVector{T},
     Σ::AbstractMatrix{T};
-    num_components::Int = size(Σ, 1),
-    ratio::T = one(T),
-    rtol::T = size(Σ, 1) * eps(T),
+    num_components::Union{Int, Nothing}=nothing,
+    vmin::Union{T, Nothing}=nothing,
+    rtol::Union{T, Nothing}=nothing,
 ) where {T<:Base.IEEEFloat}
     GeneralizedPCAcor{T}(
         collect(μ),
         collect(Σ),
         num_components = num_components,
-        ratio = ratio,
+        vmin = vmin,
         rtol = rtol,
     )
 end
