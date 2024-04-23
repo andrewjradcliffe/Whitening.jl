f4223e1ad7e57eaea8b6568eda684aab3749d7e6
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2024-04-22 19:52:12 -0700

    Update public interface and `include`s

--- src/Whitening.jl
+++ src/Whitening.jl
@@ -1,6 +1,7 @@
 module Whitening
 
 public AbstractWhiteningTransform, PCA, PCAcor, ZCA, ZCAcor, Chol
+public GeneralizedPCA, GeneralizedPCAcor
 public whiten, unwhiten, mahalanobis
 
 using LinearAlgebra
@@ -11,5 +12,7 @@ include("ZCAcor.jl")
 include("PCA.jl")
 include("PCAcor.jl")
 include("Chol.jl")
+include("GeneralizedPCA.jl")
+include("GeneralizedPCAcor.jl")
 
 end # module Whitening
