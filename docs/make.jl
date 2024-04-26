c49a0da2b97b5e1aad9f07271d8125811ccc2286
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2024-04-25 19:26:04 -0700

    Run JuliaFormatter

--- docs/make.jl
+++ docs/make.jl
@@ -1,16 +1,11 @@
 using Documenter, Whitening
 
-makedocs(
-    sitename="Whitening.jl",
-    pages = [
-        "Home" => "index.md",
-    ]
-)
+makedocs(sitename = "Whitening.jl", pages = ["Home" => "index.md"])
 
 deploydocs(
     repo = "github.com/andrewjradcliffe/Whitening.jl.git",
-    branch="gh-pages",
-    devbranch="main",
+    branch = "gh-pages",
+    devbranch = "main",
     versions = ["stable" => "v^", "v#.#", "dev" => "main"],
     devurl = "dev",
 )
